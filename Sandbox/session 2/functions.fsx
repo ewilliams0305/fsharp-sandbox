eb089cf016feee68ea13b2d93c7cdb1b5582e541
Eric Williams <ewilliams0305@gmail.com>
2024-05-08 16:04:21 -0400

    session 2

---
+++ Sandbox/session 2/functions.fsx
@@ -0,0 +1,8 @@
+
+
+/// <summary>Adds two ints together</summary>
+/// <param name="x">first value</param>
+/// <param name="y">second value</param>
+/// <returns>sum of value 1 and 2</returns>
+let add x y = x + y
+
---
+++ Sandbox/session 2/patternmatching-record.fsx
@@ -0,0 +1,31 @@
+
+type Customer = {
+    Name:string;
+    IsRegistered:bool;
+    IsEligable:bool;
+}
+
+type Spend = decimal
+type Total = decimal
+
+type CalculateTotal = (Customer * Spend) -> Total
+
+let john = { Name = "John"; IsRegistered = true; IsEligable = true }
+let mary = { Name = "Mary"; IsRegistered = true; IsEligable = false }
+let richard = { Name = "Richard"; IsRegistered = true; IsEligable = true }
+let sarah = { Name = "Sarah"; IsRegistered = true; IsEligable = true }
+
+let calculateTotal (customer:Customer, spend:Spend) : Total =    
+    let discount = 
+        match customer with 
+        | { IsRegistered = true; IsEligable = true } when spend >= 100m -> spend * 0.1m
+        | { IsRegistered = true; IsEligable = isel } when isel = false -> 0m  // pass the variable
+        | { IsRegistered = false; IsEligable = false } -> 0m
+        | _ -> 0m
+    spend - discount
+
+let assertMary = calculateTotal(mary, 99m) = 99m
+let assertJohn = calculateTotal(john, 100m) = 90m
+let assertRichard = calculateTotal(richard, 100m) = 100m
+let assertSarah = calculateTotal(sarah, 100m) = 100m
+
